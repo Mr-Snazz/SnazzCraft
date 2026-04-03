c54cbccda8cc5fe9bd870830821c9c9622acbcbb
Johnny Bliss <mr_snazz@outlook.com>
2026-04-02 21:10:00 -0700

    Voxel data that is ID specific not instance specific is now stored in new SnazzCraft::VoxelType class. This class has a private constructor and only allows the access of static instances that reside in public static member function GetVoxelType. These static instances are const and are returned as const references. This significantly reduces memory usage.

--- SnazzCraft/includes/snazzcraft-engine/world/voxel-type.hpp
+++ SnazzCraft/includes/snazzcraft-engine/world/voxel-type.hpp
@@ -4,16 +4,18 @@
 
 namespace SnazzCraft
 {
-    struct VoxelType
+    class VoxelType
     {
+    public:
         int32_t LightProducingLevel;
         int32_t LightPropogationDecrease;
         bool Cullable;
         bool CollidableToEntities;
         bool CollidableToVoxels;
 
-        VoxelType(int32_t ILightProducingLevel, int32_t ILightPropogationDecrease, bool ICullable, bool ICollidableToEntities, bool ICollidableToVoxels);
-
         static const VoxelType& GetVoxelType(uint32_t VoxelID);
+
+    private:
+        VoxelType(int32_t ILightProducingLevel, int32_t ILightPropogationDecrease, bool ICullable, bool ICollidableToEntities, bool ICollidableToVoxels);
     };
 } // SnazzCraft
